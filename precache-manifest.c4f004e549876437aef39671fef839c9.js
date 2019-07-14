498b77c831bfff30a1b8604d9e7f3e4b417aa63b
parth <parth280197@gmail.com>
2019-07-14 11:27:50 -0500

    Updates

---
+++ precache-manifest.c4f004e549876437aef39671fef839c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b62085af88704fc2379bf0761332dd",
+    "url": "/box-app/index.html"
+  },
+  {
+    "revision": "ccc9c18c1360ab733ec9",
+    "url": "/box-app/static/css/main.e736f77e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/box-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ccc9c18c1360ab733ec9",
+    "url": "/box-app/static/js/main.5a761333.chunk.js"
+  },
+  {
+    "revision": "b9dadce67fb7d361f294",
+    "url": "/box-app/static/js/runtime~main.d0a81167.js"
+  }
+]);
